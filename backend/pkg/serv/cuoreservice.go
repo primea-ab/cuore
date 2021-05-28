2215cf30ef869416bb54937931b1ffd1d02c1fb8
Tomas Haselquist <tomas@primea.se>
2021-05-28 12:14:09 +0200

    Add possibility to use override config

--- backend/pkg/serv/cuoreservice.go
+++ backend/pkg/serv/cuoreservice.go
@@ -5,13 +5,19 @@ import (
 	"lethe.se/vito/cuore/pkg/model"
 	"log"
 	"os"
+	"fmt"
 )
 
 func FetchConfig() model.Config {
-	f, err := os.Open("config/default.json")
+	f, err := os.Open("config/override.json")
 	if err != nil {
-		log.Fatal("Could not open config file")
+		fmt.Println("Could not find file override.json (Using default)")
+		f, err = os.Open("config/default.json")
+		if err != nil {
+			log.Fatal("Could not open config file")
+		}
 	}
+
 	defer f.Close()
 
 	var cfg model.Config
